332337e70a03ce1fa688ccf313ff4d4fe7a6569a
yhyahaha <yhyahaha@live.jp>
2020-02-27 20:00:38 +0900

    IKanaConverterの追加

---
+++ InterfaceKanaConverter/IKanaConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceKanaConverter
+{
+    public interface IKanaConverter
+    {
+        string ConvertToKana(string str);
+    }
+}
